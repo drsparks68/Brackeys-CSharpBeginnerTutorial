0c3f000758976eaf7a778ce60ec11de0c7c86281
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:11:04 -0700

    Initial Import

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Brackeys
+{
+    class MainClass
+    {
+        interface IItem
+        {
+            string name { get; set; }
+            int goldValue { get; set; }
+
+            void Equip();
+            void Sell();
+        }
+
+        class Sword : IItem
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+
+            public Sword (string _name)
+            {
+                name = _name;
+                goldValue = 100;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " is equipped.");
+            }
+
+            public void Sell()
+            {
+                Console.WriteLine(name + " sold for an amazing " + goldValue + " doll-hairs.");
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Sword sword = new Sword("Sword of destiny");
+            sword.Equip();
+            sword.Sell();
+            Console.ReadKey();
+        }
+    }
+}
